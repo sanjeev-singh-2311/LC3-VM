a12e1ad929818d2684dbccdb7062726fdc240f1d
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-02-19 21:38:43 +0530

    Trap routine enum and switches

--- src/main.c
+++ src/main.c
@@ -63,6 +63,16 @@ enum {
     FL_NEG = (1 << 2)  /* Negative */
 };
 
+/* The different type of trap routines available on LC-3 */
+enum {
+    TRAP_GETC  = 0x20, /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT   = 0x21, /* output a character */
+    TRAP_PUTS  = 0x22, /* output a word string */
+    TRAP_IN    = 0x23, /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT  = 0x25  /* halt the program */
+};
+
 /* Sign extend function */
 /* Used to extend lesser bit numbers to higher bits (16) */
 /* Achieved by padding Positives with 0 and Negatives with 1 to their left */
@@ -316,7 +326,29 @@ int main(int argc, char* argv[]) {
                 }
                 break;
             case OP_TRAP:
+                /* The TRAP operation or the TRAP routine allows the program to  */
+                /* stop what it is doing and go for another work like user input */
+                /* The content of PC are saved to Register 7 so the RET operation can be used */
+                {
+                    reg[R_R7] = reg[R_PC];
+                    // The trap opcode has a 8 bit (0 to 7) trapvect8 that
+                    // specifies the type of TRAP routine, so we make a switch case for that too
 
+                    switch (instr & 0xFF) {
+                        case TRAP_GETC:
+                            break;
+                        case TRAP_OUT:
+                            break;
+                        case TRAP_PUTS:
+                            break;
+                        case TRAP_IN:
+                            break;
+                        case TRAP_PUTSP:
+                            break;
+                        case TRAP_HALT:
+                            break;
+                    }
+                }
                 break;
             case OP_RES:
             case OP_RTI:
